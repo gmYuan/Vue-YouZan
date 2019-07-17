be1a4ce00be45755e158835303bb067f30462272
gmYuan <ygm121351@gmail.com>
2019-07-18 00:15:55 +0800

    引入项目结构

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -38,6 +38,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'css': '@/modules/css',
+      'js': '@/modules/js',
+      'components': '@/components'
     }
   },
   module: {
