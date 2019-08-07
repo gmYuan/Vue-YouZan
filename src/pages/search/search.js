ffa536acc54f47245c4ed562f1294638b861a64e
gmYuan <ygm121351@gmail.com>
2019-08-07 23:56:02 +0800

    完成返回顶部

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import axios from 'axios'
 import api from 'js/api.js'
 import qs from 'qs'
+import Velocity from 'velocity-animate' // 返回顶部的动画效果
 
 import mixin from 'js/mixin.js'
 
@@ -15,7 +16,8 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
   data () {
     return {
       searchList: null,
-      keyword
+      keyword,
+      showTopIcon: false
     }
   },
 
@@ -24,13 +26,25 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
       axios.post(api.searchLists, {keyword, id}).then(res => {
         this.searchList = res.data.lists
       })
+    },
+
+    showTop () {
+      let scroll = Math.abs(document.body.getBoundingClientRect().top)
+      if (scroll > 100) {
+        this.showTopIcon = true
+      } else {
+        this.showTopIcon = false
+      }
+    },
+
+    toTop () {
+      Velocity(document.body, 'scroll', {duration: 500})
     }
+
   },
 
   created () {
     this.getSearchList()
   },
-
   mixins: [mixin]
-  
 })
