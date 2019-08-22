f07b6e9145ea34084653d3d8d331cf78c0cf0706
gmYuan <ygm121351@gmail.com>
2019-08-22 23:25:37 +0800

    完成详情页轮播

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -15,23 +15,38 @@ import qs from 'qs'
 import api from 'js/api.js'
 import mixin from 'js/mixin.js'
 
+/* 引入组件 */
+import mySwipe from 'components/Swiper.vue'
+
 let {id} = qs.parse(location.search.substr(1))
 
 let app = new Vue({ // eslint-disable-line no-unused-vars
   el: '#app',
 
+  components: {
+    mySwipe
+  },
+
   data: {
     details: null,
     dealLists: null,
     tabList: ['商品详情', '本店成交'],
-    tabIndex: 0
+    tabIndex: 0,
+    bannerList: null // banner列表数据
   },
 
   methods: {
     getDetail () {
       axios.post(api.goodsDetail, {id}).then(res => {
         this.details = res.data.data
-        // console.log(this.details)
+        // 赋值banner列表
+        this.bannerList = []
+        this.details.imgs.forEach(item => {
+          this.bannerList.push({
+            clickUrl: '',
+            img: item
+          })
+        })
       })
     },
     getDeal (id) {
