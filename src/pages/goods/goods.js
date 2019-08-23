a44afe9198b3c37ca5cd34cd1a3e0b951169db20
gmYuan <ygm121351@gmail.com>
2019-08-23 08:23:59 +0800

    完成详情页弹出层处理

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -6,6 +6,7 @@ import './goods.css'
 import './goods_theme.css'
 import './goods_mars.css'
 import './goods_sku.css'
+import './goods_transition.css' // 过渡效果
 
 /* 依赖部分 */
 import Vue from 'vue'
@@ -32,7 +33,9 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
     dealLists: null,
     tabList: ['商品详情', '本店成交'],
     tabIndex: 0,
-    bannerList: null // banner列表数据
+    bannerList: null, // banner列表数据
+    skuType: 0, // 当前点击类型， 1:净含量  2: 加入购物车   3:立即购买
+    showSku: false  // 是否显示阴影和弹出层
   },
 
   methods: {
@@ -49,7 +52,7 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
         })
       })
     },
-    getDeal (id) {
+    getDeal (id) { // 获取Deal列表
       axios.post(api.goodsDeal, {id}).then(res => {
         this.dealLists = res.data.data.lists
       })
@@ -60,6 +63,22 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
       if (index) { // 只有点击切换时，才会去加载请求
         this.getDeal({id})
       }
+    },
+
+    chooseSku (curType) {
+      this.skuType = curType
+      this.showSku = true
+    }
+
+  },
+
+  watch: {
+    showSku (val, oldVal) { // 监听showSku值，设置页面是否禁止滚动(滚动穿透问题)
+      document.body.style.overflow = val ? 'hidden' : 'auto'
+      document.querySelector('html').style.overflow = val ? 'hidden' : 'auto'
+
+      document.body.style.height = val ? '100%' : 'auto'
+      document.querySelector('html').style.height = val ? '100%' : 'auto'
     }
   },
 
