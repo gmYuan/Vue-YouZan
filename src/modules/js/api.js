8cc05065012535cd84b77c1bd6e1fb76bd27be59
gmYuan <ygm121351@gmail.com>
2019-08-26 23:23:43 +0800

    完成详情页

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -5,12 +5,16 @@ let host = 'https://www.easy-mock.com/mock/5d2729c21752820de7a8a8f7/youzan'
 let url = {
   hotLists: '/index/hotLists',
   indexBanner: '/index/banner',
+
   topLists: '/category/topList',
   subLists: '/category/subList',
   subRanks: '/category/subRank',
   searchLists: '/search/list',
+
   goodsDetail: '/goods/details',
-  goodsDeal: '/goods/deal'
+  goodsDeal: '/goods/deal',
+  
+  addCart: '/cart/add'
   
 }
 
--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -29,13 +29,21 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
   },
 
   data: {
+    id: id,
+
     details: null,
     dealLists: null,
     tabList: ['商品详情', '本店成交'],
     tabIndex: 0,
     bannerList: null, // banner列表数据
+
     skuType: 0, // 当前点击类型， 1:净含量  2: 加入购物车   3:立即购买
-    showSku: false  // 是否显示阴影和弹出层
+    showSku: false,  // 是否显示阴影和弹出层
+
+    buyNum: 1, // 购买数量
+    isAddCart: false,  // 该商品是否已加入购物车
+    showAddMessage: false // 是否显示 加入购物车成功的文字提示
+
   },
 
   methods: {
@@ -68,6 +76,27 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
     chooseSku (curType) {
       this.skuType = curType
       this.showSku = true
+    },
+
+    // 修改购买商品的数量
+    changeBuyNum (num) {
+      if (this.buyNum === 1 && num < 0) { return }
+      this.buyNum = this.buyNum += num
+    },
+
+    // 点击添加购物车: 发起请求存储商品到购物车 + 隐藏弹层 + 显示购物车小图标 + 显示成功文案
+    addCart () {
+      axios.post(api.addCart, {id, number: this.buyNum}).then(res => {
+        if (res.data.status == 200) {
+          this.showSku = false
+          this.isAddCart = true
+          this.showAddMessage = true
+
+          setTimeout(() => {
+            this.showAddMessage = false
+          }, 2000)
+        }
+      })
     }
 
   },
