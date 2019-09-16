b913bedd26f5027cc57f8baa6178da699356f974
gmYuan <ygm121351@163.com>
2019-09-16 15:18:22 +0800

    完成勾选状态联动

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -14,7 +14,30 @@ import api from 'js/api.js'
 let app = new Vue({ // eslint-disable-line no-unused-vars
   el: '.container',
 
-  computed: {},
+  computed: {
+    allSelected: {
+      get(){     // Q3 勾选所有店铺后，自动勾选全选按钮
+        if (this.cartLists && this.cartLists.length > 0) {
+          return this.cartLists.every(shop => {
+            return shop.isChecked
+          })
+        }
+        return false
+      },
+      set(newValue){   // Q5  勾选全选后，自动勾选店铺A按钮 和 店铺A下所有商品
+        if (this.cartLists && this.cartLists.length > 0) {
+          this.cartLists.forEach(shop => {
+            shop.isChecked = newValue
+            shop.goodsList.forEach(good => {
+              good.isChecked = newValue
+            })
+          })
+        }
+      }
+    }
+
+  },
+
   data: {
     cartLists: null,
   },
@@ -35,11 +58,22 @@ let app = new Vue({ // eslint-disable-line no-unused-vars
       })
     },
    
-    changeCheck(shop, good) {           // 切换商品勾选状态 + 对应店铺状态
+    selectGood(shop, good) {           //Q2 切换商品勾选状态 + 对应店铺状态
       good.isChecked = !good.isChecked
       shop.isChecked =  shop.goodsList.every(good => {
         return good.isChecked == true
       })
+    },
+
+    selectShop(shop) {  // Q4 切换店铺勾选状态 + 对应下商品状态
+      shop.isChecked = !shop.isChecked
+      shop.goodsList.forEach(good => {
+        good.isChecked = shop.isChecked
+      })
+    },
+
+    selectAll() {
+      this.allSelected = !this.allSelected
     }
 
   },
