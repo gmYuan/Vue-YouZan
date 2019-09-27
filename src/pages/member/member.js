628abc8d9c427dbef0c837b9fcd0543143dae7f0
gmYuan <ygm121351@163.com>
2019-09-27 18:37:58 +0800

    vuex重构地址

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,43 +1,6 @@
 
-// S1 导入路由组件
 import Vue from 'vue'
-import Router from 'vue-router'
-Vue.use(Router)
-
-// S2 配置路由属性
-const routes = [{
-    path: '/',
-    component: ()=> import('./components/member.vue')
-  },
-  {
-    path: '/address',
-    component: ()=> import('./components/address/address.vue'),
-    children: [
-      {
-        path: '',
-        redirect: 'addressLists'
-
-      },
-      {
-        path: 'addressLists',
-        name: 'addressLists',
-        component: ()=> import('./components/address/addressList.vue'),
-      },
-      {
-        path: 'addressEdit',
-        name: 'addressEdit',
-        component: ()=> import('./components/address/addressEdit.vue'),
-      }
-    ]
-
-  }
-]
-
-
-//S3 创建路由实例
-const router = new Router({
-  routes
-})
+import router from '@/router/index'
 
 
 // S4 根组件注入
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+// S1 导入路由组件
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+// S2 配置路由属性
+const routes = [{
+    path: '/',
+    component: ()=> import('@/pages/member/components/member.vue')
+  },
+  {
+    path: '/address',
+    component: ()=> import('@/pages/member/components/address/address.vue'),
+    children: [
+      {
+        path: '',
+        redirect: 'addressLists'
+
+      },
+      {
+        path: 'addressLists',
+        name: 'addressLists',
+        component: ()=> import('@/pages/member/components/address/addressList.vue'),
+      },
+      {
+        path: 'addressEdit',
+        name: 'addressEdit',
+        component: ()=> import('@/pages/member/components/address/addressEdit.vue'),
+      }
+    ]
+
+  }
+]
+
+
+//S3 创建路由实例
+const router = new Router({
+  routes
+})
+
+
+export default router
